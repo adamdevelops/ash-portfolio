5efd7d97d9b60168ed7094b87a03e26dd45f3fce
adamdevelops <adamh93@verizon.net>
2025-08-04 18:19:08 -0400

    done: Initial commit for basic layout

---
+++ src/app/components/header.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </>
+  )
+}
